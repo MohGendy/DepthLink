08eebfa98249b83e9ea37fdf282c9eabaff29b82
MohGendy <midoomidoooo12@gmail.com>
2025-06-28 21:41:12 +0300

    our project

--- frontend/assets/js/main.js
+++ frontend/assets/js/main.js
@@ -1,6 +1,6 @@
 // import {io} from "../vendor/socket/client-dist/socket.io"
 // const {io} = require('../vendor/socket/client-dist/socket.io');
-const socket = io("http://localhost:3030");
+const socket = io("");
 
 socket.on("connect", () => {
   console.log("connected");
